dd77db1de87d7d4058cf43ec0a21ceb76771e811
shad0wfax-alfa <tindari_rathe@yahoo.com>
2016-01-10 22:26:28 -0800

    Updated compile.bat to reflect new host location.

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 del *.ncs
 del *.ndb
-NWNScriptCompiler.exe -e -v1.70 -o -h "C:\Users\dnd\Documents\Neverwinter Nights 2" -m alfa010_bg .\*.nss -y
+NWNScriptCompiler.exe -e -v1.70 -o -h "C:\Users\ALFA-009\Documents\Neverwinter Nights 2 BG" -m alfa010_bg .\*.nss -y
