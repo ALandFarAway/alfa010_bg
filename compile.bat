5e08b0b13495fbbfd70539543fba705eaa0b40e4
shad0wfax-alfa <tindari_rathe@yahoo.com>
2018-04-11 18:46:15 -0700

    compile.bat output to desktop "out_bg.txt" for debugging purposes.

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 del *.ncs
 del *.ndb
-NWNScriptCompiler.exe -e -v1.70 -o -h "C:\Users\ALFA-009\Documents\Neverwinter Nights 2 BG" -m alfa010_bg .\*.nss -y
+NWNScriptCompiler.exe -e -v1.70 -o -h "C:\Users\ALFA-009\Documents\Neverwinter Nights 2 BG" -m alfa010_bg .\*.nss -y > C:\Users\ALFA-009\Desktop\out_bg.txt
